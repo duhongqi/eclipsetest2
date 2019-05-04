7826061b390ce5a87f53e894962e2d78e7e02982
dhq <1980954392@qq.com>
2019-05-05 05:46:59 +0800

    再次修改

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
  
 	public static void main(String[] args) {
-		System.out.println("22222");
+		System.out.println("4444");
 		System.out.println("33333");
 		SpringApplication.run(App.class, args);
 	}
